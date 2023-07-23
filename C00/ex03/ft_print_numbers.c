fec4df977e1aa1069b2d4c154ae0426453c7fe42
Jaemin Kim <jaemikim@c8r2s12.42seoul.kr>
2023-07-23 16:00:37 +0900

    fuck

--- C00/ex03/ft_print_numbers.c
+++ C00/ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: jaemikim <jaemikim@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:13:33 by jaemikim          #+#    #+#             */
-/*   Updated: 2023/07/20 16:24:46 by jaemikim         ###   ########.fr       */
+/*   Updated: 2023/07/23 15:57:48 by jaemikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_print_numbers(void)
 {
 	char	a;
 
-	a = '1';
+	a = '0';
 	while (a <= '9' )
 	{
 		write(1, &a, 1);
